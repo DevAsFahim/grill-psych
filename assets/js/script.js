ef34b7ee054ab2fe67a8efaf7c704d3bea9aa096
DevAsFahim <devasfahim@gmail.com>
2025-10-30 12:54:07 +0600

    loader added

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Preload
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
